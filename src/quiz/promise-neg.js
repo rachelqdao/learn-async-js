6cb4717fb8cbbcb09e20cfdd259745930e292671
Rachel Dao <rachelqdao@gmail.com>
2024-02-27 13:23:58 -0500

    in class activities

--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -3,3 +3,12 @@ const array2D = [
     [4, 5, 6],
     [7, 8, -9]
 ];
+
+
+// use promise.any, similar to promise-neg
+
+// push to an array
+let negsPerRowPromise = [];
+
+
+// similar then catch block to the other example
--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -3,3 +3,21 @@ const array2D = [
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+// sum each row concurrently
+// add the results of each row
+function sumOfARow(arr, rowIdx) {
+    return new Promise((resolve, reject) => {
+        
+    })
+}
+
+// use promise.all to resolve the list of all promises
+Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
+.then((responses) => {
+  for (const response of responses) {
+
+  }
+})
+.catch((error) => console.log(`Error Msh: ${error}`))
+
